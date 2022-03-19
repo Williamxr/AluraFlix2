bd1860c87b68bcf4a192519efeec82129295d354
William <william.teletabs@hotmail.com>
2022-03-19 10:30:31 -0300

    Criando uma segunda função para o código ficar mais limpo e semântico

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 function adicionarFilme() {
-  var campoFilmeFavorito = document.getElementById("filme").value; //pegando o valor da caixa input
-  var elementoFilmeFavorito = "<img src=" + campoFilmeFavorito + ">"; //selecionando como a imagem vai aparecer na tela
+  var filmeFavorito = document.getElementById("filme").value; //pegando o valor da caixa input
+  if (filmeFavorito.endsWith(".jpg")) {
+    //se o filme terminar com .jpg, a função será executada normalmente
+
+    listarFilmesNaTela(filmeFavorito);
+  } else {
+  }
+  document.getElementById("filme").value = ""; //limpando o campo de digitação depois que o filme for adicionado
+}
+
+function listarFilmesNaTela(filmeFavorito) {
+  var elementoFilmeFavorito = "<img src=" + filmeFavorito + ">"; //selecionando como a imagem vai aparecer na tela
   var elementoListaFilmes = document.getElementById("listaFilmes"); //pegando o id 'listaFilmes' para adicionar o filme na tela quando o botao for clicado
   elementoListaFilmes.innerHTML =
     elementoListaFilmes.innerHTML + elementoFilmeFavorito; //adicionando filme na tela sem que uma imagem se sobreponha sobre a outra
-
-  campoFilmeFavorito.value = ""; //limpando o campo de digitação depois que o filme for adicionado
 }
